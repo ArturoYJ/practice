dcadf926b13e9619b6c7265260de4e88b91cc5a3
Aurora <243707@ids.upchiapas.edu.mx>
2025-10-31 22:26:24 -0600

    Iniciar registro y menú

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './menu/sidebar/sidebar';
+import { NavbarComponent } from './menu/navbar/navbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
+  template: `
+    <div class="app-layout">
+      <app-navbar></app-navbar>
+      <app-sidebar></app-sidebar>
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+  `,
   styleUrl: './app.css'
 })
 export class App {
