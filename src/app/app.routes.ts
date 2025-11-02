ce6a08190171636e7acedcd630a3bc4665b188d3
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-02 16:27:11 -0600

    Implementar ruteo para dashboard.view y animals-table.view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PrivateLayout } from './layouts/private.layout/private.layout';
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./layouts/private.layout/private.layout')
+            .then(m => m.PrivateLayout),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./features/dashboard/dashboard.view/dashboard.view')
+                    .then(m => m.DashboardView)
+            },
+            {
+                path: 'animals',
+                loadChildren: () => import('./features/animals/animals.routes')
+                    .then(m => m.animalRoutes)
+            }
+        ]
+    }
+];
