5e2e487098aae42f6776034ba50703aa2700c9a6
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-02 16:19:18 -0600

    Corregir semántica de componentes compartidos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header.component/header.component';
-import { AsideComponent } from "./shared/components/aside.component/aside.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AsideComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hugin_munin_ng');
 }
