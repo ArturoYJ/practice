60a9ef2d0f38910d69569156be7847d8636d4013
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-03 19:09:09 -0600

    Agregar vista de registro para nuevo ejemplar

--- src/app/features/animals/animals-table.view/animals-table.view.ts
+++ src/app/features/animals/animals-table.view/animals-table.view.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-animals-table.view',
-  imports: [MatIcon],
+  imports: [MatIcon, RouterLink],
   templateUrl: './animals-table.view.html',
   styleUrl: './animals-table.view.css',
 })
--- src/app/features/animals/animals.routes.ts
+++ src/app/features/animals/animals.routes.ts
@@ -5,5 +5,10 @@ export const animalRoutes: Routes = [
         path: '',
         loadComponent: () => import('./animals-table.view/animals-table.view')
             .then(m => m.AnimalsTableView)
+    },
+    {
+        path: 'register_animal',
+        loadComponent: () => import('./register-form.view/register-form.view')
+            .then(m => m.RegisterFormView)
     }
 ];
